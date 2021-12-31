a2ef9dbd1b1033eb6db45c02829ebd24dd1a2de8
jbyte23 <janbajt@gmail.com>
2021-12-31 10:06:42 +0100

    httpclien link fix

--- services/src/main/java/si/fri/rso/skupina3/rv_park_tenancy/services/beans/RvParkTenancyBean.java
+++ services/src/main/java/si/fri/rso/skupina3/rv_park_tenancy/services/beans/RvParkTenancyBean.java
@@ -40,9 +40,9 @@ public class RvParkTenancyBean {
     @PostConstruct
     private void init() {
         httpClient = ClientBuilder.newClient();
-        billBaseUrl = "http://localhost:8087/v1/park_bills";
+//        billBaseUrl = "http://localhost:8087/v1/park_bills";
 //        parkBaseUrl = "http://localhost:8081/v1/parks";
-//        billBaseUrl = "http://20.72.172.42/billing/v1/park_bills";
+        billBaseUrl = "http://20.72.172.42/billing/v1/park_bills";
         parkBaseUrl = "http://20.72.172.42/parks/v1/parks";
     }
 
